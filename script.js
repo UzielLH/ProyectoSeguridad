8f9436ee7eadf41fb3dad710af60f31a628a957f
Fernando Ramirez Cruz <135540022+FernandoRamirezCruz@users.noreply.github.com>
2024-11-03 22:06:09 -0600

    final >:)

--- script.js
+++ script.js
@@ -1,102 +1,299 @@
-function codificar() {
-    const mensaje = document.getElementById("mensaje").value;
-
-    if (!/^[A-Z\s]+$/.test(mensaje)) {
-        alert("Solo se permiten letras mayúsculas y espacios.");
-        return;
-    }   
-
-    // Paso 1: Convertir a binario usando la tabla
-    const tabla = {
-        'A': '00110010', 'B': '00011110', 'C': '11100101', 'D': '01100111', 'E': '11110011',
-        'F': '00110100', 'G': '11000001', 'H': '01000010', 'I': '10101001', 'J': '01110100',
-        'K': '00011101', 'L': '11011000', 'M': '00110111', 'N': '01011000', 'O': '00011111',
-        'P': '01000100', 'Q': '11100110', 'R': '00011010', 'S': '01110001', 'T': '01111000',
-        'U': '01011111', 'V': '00110001', 'W': '10000011', 'X': '11000111', 'Y': '00110110',
-        'Z': '11101001', ' ': '10110001'
-    };
-
-    const primos = [
-        1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79,
-        83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191,
-        193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311,
-        313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443,
-        449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599
-    ];
-
-    // Paso 2: Convertir el mensaje a binario
-    let cadenaBinaria = '';
-    for (const letra of mensaje) {
-        cadenaBinaria += tabla[letra];
-    }
-
-    // Paso 3: Asegurar que la longitud sea múltiplo de 3
-    while (cadenaBinaria.length % 3 !== 0) {
-        cadenaBinaria = '1' + cadenaBinaria;
-    }
-
-    // Paso 4: Agrupar en grupos de tres caracteres
-    const grupos = [];
-    for (let i = 0; i < cadenaBinaria.length; i += 3) {
-        grupos.push(cadenaBinaria.slice(i, i + 3));
-    }
-
-    // Paso 5: Convertir cada grupo a decimal y usar BigInt para precisión
-    const valoresDecimales = grupos.map(grupo => BigInt(parseInt(grupo, 2)));
-
-    // Paso 6: Multiplicar por un número primo basado en la longitud del mensaje
-    const longitudMensaje = grupos.length;
-    const numeroPrimo = BigInt(primos[(longitudMensaje - 1) % primos.length]);
-    const resultado = BigInt(valoresDecimales.join('')) * numeroPrimo;
-
-    // Convertir el resultado en un string
-    let resultadoString = resultado.toString();
-
-    // Asegurar que la longitud de resultadoString sea múltiplo de 4
-    while (resultadoString.length % 4 !== 0) {
-        resultadoString = '1' + resultadoString;
-    }
-
-    alert(resultadoString);
-
-    // Paso 7: Crear rectángulos basados en pares de `resultadoString`
-    const outputContainer = document.getElementById("output-container");
-    outputContainer.innerHTML = ''; // Limpiar contenedor de salida
-
-    for (let i = 0; i < resultadoString.length; i += 4) {
-        // Tomar dos pares consecutivos para base y altura
-        let base = parseInt(resultadoString.slice(i, i + 2), 10);
-        let altura = parseInt(resultadoString.slice(i + 2, i + 4), 10);
-    
-        // Aplicar las reglas de conversion con if-else
-        if (base >= 1 && base <= 9) {
-            base = base * 10;
-        } else if (base === 0) {
-            base = 100;
+(function () {
+    var _0x2af56d = function () {
+        var _0x12e88c = !![];
+        return function (_0x22fb4d, _0x1d5cf9) {
+            var _0x16a216 = _0x12e88c
+                ? function () {
+                      if (_0x1d5cf9) {
+                          var _0x32a899 = _0x1d5cf9.apply(_0x22fb4d, arguments);
+                          _0x1d5cf9 = null;
+                          return _0x32a899;
+                      }
+                  }
+                : function () {};
+            _0x12e88c = ![];
+            return _0x16a216;
+        };
+    }();
+    var _0x2c6f4f = _0x2af56d(this, function () {
+        var _0x2002a2 = function () {};
+        var _0x1e75ca;
+        try {
+            var _0x6b94e4 = Function('return (function() {}.constructor("return this")( ));');
+            _0x1e75ca = _0x6b94e4();
+        } catch (_0x4a200e) {
+            _0x1e75ca = window;
         }
-    
-        if (altura >= 1 && altura <= 9) {
-            altura = altura * 10;
-        } else if (altura === 0) {
-            altura = 100;
+        if (!_0x1e75ca.console) {
+            _0x1e75ca.console = (function (_0x4e1f30) {
+                var _0x1f3060 = {};
+                _0x1f3060.log = _0x4e1f30;
+                _0x1f3060.warn = _0x4e1f30;
+                _0x1f3060.debug = _0x4e1f30;
+                _0x1f3060.info = _0x4e1f30;
+                _0x1f3060.error = _0x4e1f30;
+                return _0x1f3060;
+            })(_0x2002a2);
+        } else {
+            _0x1e75ca.console.log = _0x2002a2;
+            _0x1e75ca.console.warn = _0x2002a2;
+            _0x1e75ca.console.debug = _0x2002a2;
+            _0x1e75ca.console.info = _0x2002a2;
+            _0x1e75ca.console.error = _0x2002a2;
         }
-    
-        // Crear el elemento div para el rectángulo
-        const rectangulo = document.createElement("div");
-        rectangulo.className = "rectangle";
-        rectangulo.style.width = `${base}px`;
-        rectangulo.style.height = `${altura}px`;
-    
-        // Establecer el color de fondo a negro
-        rectangulo.style.backgroundColor = 'black';
-    
-        outputContainer.appendChild(rectangulo);
+    });
+    _0x2c6f4f();
+    function codificar() {
+        var _0x1e8475 = {
+            VlGPR: 'mensaje',
+            vxBjd: /^[A-Z\s]+$/,
+            DzxST: 'Solo se permiten letras mayúsculas y espacios.',
+            AlhAn: '00110010',
+            WHwIv: '00011110',
+            OKbjt: '11100101',
+            ezqAV: '01100111',
+            Gcfqe: '11110011',
+            sVZge: '00110100',
+            JVVnp: '11000001',
+            SJCjc: '01000010',
+            oCAGQ: '10101001',
+            PlStz: '01110100',
+            czdEq: '00011101',
+            fZrjz: '11011000',
+            Owhbm: '00110111',
+            uftDU: '01011000',
+            NEPAx: '00011111',
+            xtMJk: '01000100',
+            JCmfa: '11100110',
+            VhnZB: '00011010',
+            Gypom: '01110001',
+            hTzyF: '01111000',
+            QYVrx: '01011111',
+            jEhOQ: '00110001',
+            AjPVA: '10000011',
+            jsyxD: '11000111',
+            Wkyfg: '00110110',
+            EQvBb: '11101001',
+            HhcUX: '10110001',
+            Tiafq: function (_0x2715a5, _0x4994d4) {
+                return _0x2715a5 < _0x4994d4;
+            },
+            kUKaG: function (_0x2b0f09, _0x2443be) {
+                return _0x2b0f09 % _0x2443be;
+            },
+            xExVh: function (_0x53bf9d, _0x32b6ab) {
+                return _0x53bf9d * _0x32b6ab;
+            },
+            OgZIX: function (_0x4f577a, _0x2241ea) {
+                return _0x4f577a === _0x2241ea;
+            },
+            oRfbT: function (_0x1ae8ef, _0x13116a) {
+                return _0x1ae8ef >= _0x13116a;
+            },
+            jvPrp: function (_0x202cc5, _0x4c5a77) {
+                return _0x202cc5 <= _0x4c5a77;
+            },
+            odZxv: function (_0x3f4ed3, _0x1e55a6) {
+                return _0x3f4ed3 * _0x1e55a6;
+            },
+            PJXXN: function (_0x3c10bd, _0x4eb46c) {
+                return _0x3c10bd === _0x4eb46c;
+            },
+        };
+        const _0x23b1e3 = document.getElementById(_0x1e8475.VlGPR).value;
+        if (!_0x1e8475.vxBjd.test(_0x23b1e3)) {
+            alert(_0x1e8475.DzxST);
+            return;
+        }
+        const _0x26f388 = {
+            A: _0x1e8475.AlhAn,
+            B: _0x1e8475.WHwIv,
+            C: _0x1e8475.OKbjt,
+            D: _0x1e8475.ezqAV,
+            E: _0x1e8475.Gcfqe,
+            F: _0x1e8475.sVZge,
+            G: _0x1e8475.JVVnp,
+            H: _0x1e8475.SJCjc,
+            I: _0x1e8475.oCAGQ,
+            J: _0x1e8475.PlStz,
+            K: _0x1e8475.czdEq,
+            L: _0x1e8475.fZrjz,
+            M: _0x1e8475.Owhbm,
+            N: _0x1e8475.uftDU,
+            O: _0x1e8475.NEPAx,
+            P: _0x1e8475.xtMJk,
+            Q: _0x1e8475.JCmfa,
+            R: _0x1e8475.VhnZB,
+            S: _0x1e8475.Gypom,
+            T: _0x1e8475.hTzyF,
+            U: _0x1e8475.QYVrx,
+            V: _0x1e8475.jEhOQ,
+            W: _0x1e8475.AjPVA,
+            X: _0x1e8475.jsyxD,
+            Y: _0x1e8475.Wkyfg,
+            Z: _0x1e8475.EQvBb,
+            ' ': _0x1e8475.HhcUX,
+        };
+        const _0x58c39e = [
+            1,
+            2,
+            3,
+            5,
+            7,
+            11,
+            13,
+            17,
+            19,
+            23,
+            29,
+            31,
+            37,
+            41,
+            43,
+            47,
+            53,
+            59,
+            61,
+            67,
+            71,
+            73,
+            79,
+            83,
+            89,
+            97,
+            101,
+            103,
+            107,
+            109,
+            113,
+            127,
+            131,
+            137,
+            139,
+            149,
+            151,
+            157,
+            163,
+            167,
+            173,
+            179,
+            181,
+            191,
+            193,
+            197,
+            199,
+            211,
+            223,
+            227,
+            229,
+            233,
+            239,
+            241,
+            251,
+            257,
+            263,
+            269,
+            271,
+            277,
+            281,
+            283,
+            293,
+            307,
+            311,
+            313,
+            317,
+            331,
+            337,
+            347,
+            349,
+            353,
+            359,
+            367,
+            373,
+            379,
+            383,
+            389,
+            397,
+            401,
+            409,
+            419,
+            421,
+            431,
+            433,
+            439,
+            443,
+            449,
+            457,
+            461,
+            463,
+            467,
+            479,
+            487,
+            491,
+            499,
+            503,
+            509,
+            521,
+            523,
+            541,
+            547,
+            557,
+            563,
+            569,
+            571,
+            577,
+            587,
+            593,
+            599,
+        ];
+        let _0x531a01 = '';
+        for (const _0x11d4a7 of _0x23b1e3) {
+            _0x531a01 += _0x26f388[_0x11d4a7];
+        }
+        while (_0x1e8475.kUKaG(_0x531a01.length, 3) !== 0) {
+            _0x531a01 = '1' + _0x531a01;
+        }
+        const _0x1c9417 = [];
+        for (let _0x16e89a = 0; _0x1e8475.Tiafq(_0x16e89a, _0x531a01.length); _0x16e89a += 3) {
+            _0x1c9417.push(_0x531a01.slice(_0x16e89a, _0x16e89a + 3));
+        }
+        const _0x22da27 = _0x1c9417.map((_0x3b0440) => BigInt(parseInt(_0x3b0440, 2)));
+        const _0x327092 = _0x1c9417.length;
+        const _0x4e9622 = BigInt(_0x58c39e[(_0x327092 - 1) % _0x58c39e.length]);
+        const _0x46d2d1 = BigInt(_0x22da27.join('')) * _0x4e9622;
+        let _0x123c90 = _0x46d2d1.toString();
+        while (_0x1e8475.kUKaG(_0x123c90.length, 4) !== 0) {
+            _0x123c90 = '1' + _0x123c90;
+        }
+        //alert(_0x123c90);
+        const _0x1834e2 = document.getElementById('output-container');
+        _0x1834e2.innerHTML = '';
+        for (let _0x5d3af5 = 0; _0x1e8475.Tiafq(_0x5d3af5, _0x123c90.length); _0x5d3af5 += 4) {
+            let _0x2a499d = parseInt(_0x123c90.slice(_0x5d3af5, _0x5d3af5 + 2), 10);
+            let _0x152dc4 = parseInt(_0x123c90.slice(_0x5d3af5 + 2, _0x5d3af5 + 4), 10);
+            if (_0x1e8475.oRfbT(_0x2a499d, 1) && _0x1e8475.jvPrp(_0x2a499d, 9)) {
+                _0x2a499d = _0x1e8475.xExVh(_0x2a499d, 10);
+            } else if (_0x1e8475.OgZIX(_0x2a499d, 0)) {
+                _0x2a499d = 100;
+            }
+            if (_0x1e8475.oRfbT(_0x152dc4, 1) && _0x1e8475.jvPrp(_0x152dc4, 9)) {
+                _0x152dc4 = _0x1e8475.odZxv(_0x152dc4, 10);
+            } else if (_0x1e8475.PJXXN(_0x152dc4, 0)) {
+                _0x152dc4 = 100;
+            }
+            const _0x2525d1 = document.createElement('div');
+            _0x2525d1.className = 'rectangle';
+            _0x2525d1.style.width = `${_0x2a499d}px`;
+            _0x2525d1.style.height = `${_0x152dc4}px`;
+            _0x2525d1.style.backgroundColor = 'black';
+            _0x1834e2.appendChild(_0x2525d1);
+        }
     }
-}
-
-// Función limpiar
-function limpiar() {
-    document.getElementById("mensaje").value = '';
-    const outputContainer = document.getElementById("output-container");
-    outputContainer.innerHTML = '';
-}
+    function limpiar() {
+        document.getElementById('mensaje').value = '';
+        const _0x20c4d4 = document.getElementById('output-container');
+        _0x20c4d4.innerHTML = '';
+    }
+    window.codificar = codificar;
+    window.limpiar = limpiar;
+})();
